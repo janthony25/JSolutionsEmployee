04d4ae926c3c070d963e121b24812daf08ab7042
janthony25 <junanthony.business@gmail.com>
2024-02-21 19:03:04 +1300

    Add ApplicationDbContext

---
+++ JSolutionsEmployee/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JSolutionsEmployee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JSolutionsEmployee.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> employees { get; set; }
+    }
+}
