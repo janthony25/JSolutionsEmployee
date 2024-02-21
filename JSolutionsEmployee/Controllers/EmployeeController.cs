6ad13122ff112d3ed85d9193b5ae2447e9563acb
janthony25 <junanthony.business@gmail.com>
2024-02-21 21:55:30 +1300

    Add Update Controller

--- JSolutionsEmployee/Controllers/EmployeeController.cs
+++ JSolutionsEmployee/Controllers/EmployeeController.cs
@@ -34,5 +34,41 @@ namespace JSolutionsEmployee.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.employees.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.employees.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+
     }
 }
